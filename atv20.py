92d2eb43e689b4cd8e97e107523ee6508f2129d9
emanuel378 <emanuelmvieira10@gmail.com>
2024-09-25 13:29:26 -0300

    soma de pares

---
+++ atv20.py
@@ -0,0 +1,12 @@
+# Crie um programa que utilize uma estrutura de repetição para somar todos os números pares de 1 a 100 e exiba o resultado.
+soma= 0
+for p in range(1,101):
+    if p%2 == 0:
+        soma=soma + p
+
+print(soma)
+        
+
+    
+
+    
